7d61f638caf46829262f570e4e3cfcb277fe0817
Piotr Walat <pwalat98@gmail.com>
2022-04-11 20:42:41 +0200

    draft of draft

---
+++ Assets/Scripts/CarBehaviourBase.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CarBehaviourBase : MonoBehaviour
+    {
+        // Method responsible for increasing speed
+        private void Accelerate()
+        {
+
+        }
+
+        // Method responsible for decreasing speed
+        private void Breake()
+        {
+
+        }
+
+        // Method responsible for detecting car from the same line
+        private Tuple<GameObject, float> DetectCarAheadWithDistance()
+        {
+            return null;
+        }
+    }
+}
---
+++ NonUnityClasses/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace NonUnityClasses
+{
+    public static class Constants
+    {
+        public enum CarStates
+        {
+            Breaking,
+            WaitingForGreen,
+            Stop,
+            Accelerating,
+            FollowingPath,
+            FollowingNextCar
+        }
+
+        public const float acceleration = 1.0f;
+        public const float breaking = 2.0f;
+
+        public const float minFirstCarDelay = 9.99f; //placeholder
+        public const float maxFirstCarDelay = 9.99f; //placeholder
+
+        public const float minSecondCarDelay = 9.99f; //placeholder
+        public const float maxSecondCarDelay = 9.99f; //placeholder
+    }
+}
