5215203cf8d4d25fe77d2d28b17ff92f3980c2c7
Piotr Walat <pwalat98@gmail.com>
2022-07-05 21:41:02 +0200

    populating light timers with ui

---
+++ Assets/Scripts/LightEnabler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LightEnabler : MonoBehaviour
+{
+    public GameObject lightInputPrefab;
+    public GameObject lightContainerPrefab;
+
+    public GameObject lightInput = null;
+
+    void OnMouseDown()
+    {
+        if (lightInput == null)
+        {
+            Debug.Log("clicked");
+            lightInput = Instantiate(lightInputPrefab, transform.position, Quaternion.identity);
+            lightInput.transform.parent = GameObject.Find("Canvas").transform;
+            lightInput.GetComponent<LightParametersContainer>().lightToPopulate = this.gameObject;
+            lightInput.GetComponent<RectTransform>().sizeDelta = lightInputPrefab.GetComponent<RectTransform>().sizeDelta;
+            lightInput.GetComponent<RectTransform>().localScale = lightInputPrefab.GetComponent<RectTransform>().localScale;
+            lightInput.GetComponent<RectTransform>().anchoredPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+        }
+    }
+
+    void OnExitButtonClick()
+    {
+        Destroy(lightInput);
+        lightInput = null;
+    }
+}
---
+++ Assets/Scripts/LightParametersContainer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class LightParametersContainer : MonoBehaviour
+{
+    public GameObject lightToPopulate;
+    public TMP_InputField green;
+    public TMP_InputField yellow;
+    public TMP_InputField offfset;
+
+    public void OnButtonDown()
+    {
+        lightToPopulate.GetComponent<LightChangerTimer>().greenTime = float.Parse(green.text);
+        lightToPopulate.GetComponent<LightChangerTimer>().yellowTime = float.Parse(yellow.text);
+        lightToPopulate.GetComponent<LightChangerTimer>().offset = float.Parse(offfset.text);
+    }
+
+    // TODO - remove as it is temp
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            lightToPopulate.GetComponent<LightChangerTimer>().greenTime = float.Parse(green.text);
+            lightToPopulate.GetComponent<LightChangerTimer>().yellowTime = float.Parse(yellow.text);
+            lightToPopulate.GetComponent<LightChangerTimer>().offset = float.Parse(offfset.text);
+        }
+    }
+}
