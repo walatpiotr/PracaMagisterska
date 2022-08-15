769393bcf99ae0765a6f159f0da0d63813535664
Piotr Walat <pwalat98@gmail.com>
2022-08-15 15:38:14 +0200

    working input file path

--- Assets/JsonParser.cs
+++ Assets/JsonParser.cs
@@ -6,31 +6,40 @@ using UnityEngine;
 
 public class JsonParser : MonoBehaviour
 {
+    public TMP_InputField inputPath;
+
     public void LoadJson()
     {
-        using (StreamReader r = new StreamReader("config/config.json"))
+        try
         {
-            string json = r.ReadToEnd();
-            Configuration config = JsonConvert.DeserializeObject<Configuration>(json);
-            var configObject = GameObject.FindGameObjectWithTag("configuration").GetComponent<Configuration>();
+            using (StreamReader r = new StreamReader(inputPath.text.ToString()))
+            {
+                string json = r.ReadToEnd();
+                Configuration config = JsonConvert.DeserializeObject<Configuration>(json);
+                var configObject = GameObject.FindGameObjectWithTag("configuration").GetComponent<Configuration>();
 
-            configObject.acceleration = config.acceleration;
-            configObject.breaking = config.breaking;
+                configObject.acceleration = config.acceleration;
+                configObject.breaking = config.breaking;
 
-            configObject.ffchance = config.ffchance;
-            configObject.fschance = config.fschance;
-            configObject.sschance = config.sschance;
-            configObject.sfchance = config.sfchance;
+                configObject.ffchance = config.ffchance;
+                configObject.fschance = config.fschance;
+                configObject.sschance = config.sschance;
+                configObject.sfchance = config.sfchance;
 
-            configObject.minFirstCarDelay = config.minFirstCarDelay;
-            configObject.maxFirstCarDelay = config.maxFirstCarDelay;
+                configObject.minFirstCarDelay = config.minFirstCarDelay;
+                configObject.maxFirstCarDelay = config.maxFirstCarDelay;
 
-            configObject.minSecondCarDelay = config.minSecondCarDelay;
-            configObject.maxSecondCarDelay = config.maxSecondCarDelay;
+                configObject.minSecondCarDelay = config.minSecondCarDelay;
+                configObject.maxSecondCarDelay = config.maxSecondCarDelay;
 
-            configObject.safeDistance = config.safeDistance;
-            configObject.maxVelocity = config.maxVelocity;
-            configObject.generatorDistance = config.generatorDistance;
+                configObject.safeDistance = config.safeDistance;
+                configObject.maxVelocity = config.maxVelocity;
+                configObject.generatorDistance = config.generatorDistance;
+            }
+        }
+        catch
+        {
+            inputPath.text = "Something went wrong! Please provide correct path";
         }
     }
 }
